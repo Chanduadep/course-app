ef33a49c297432dffa5cef764e2365b8cea53c15
Chanduadep <adepchandu22@gmail.com>
2025-02-02 00:39:35 +0530

    course-app

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
-// require('dotenv').config()
+const dotenv=require('dotenv')
+dotenv.config()
 
 const connectDB = async () => {
     try {
-      await mongoose.connect('mongodb+srv://adep222004:XhF9wJtrvM1m6BHS@cluster0.qzrbc.mongodb.net/courseapp');
+      await mongoose.connect(process.env.MONGODB_URL);
       console.log('MongoDB Connected');
     } catch (err) {
       console.error('Database connection error:', err.message);
